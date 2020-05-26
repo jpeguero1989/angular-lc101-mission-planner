68ad4fa083553b6e0364aee63eebd6ead18bf477
Elvin Peguero <jpeguero1989@gmail.com>
2020-05-26 14:43:30 -0400

    before the bonus mission

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,16 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
+  memberBeingEdited: object = null;
 
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+  isAMember: boolean= false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(memberName: string, isFirst: boolean) {
+
+    for (let i=0;i<this.crew.length;i++)
+    {
+      if ((this.crew[i]['name']=== memberName))
+      {
+      this.isAMember = true;
+      }
+    }
+
+    if (!this.isAMember)
+    {
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+    this.isAMember = false;
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+}
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
   }
 
 }
